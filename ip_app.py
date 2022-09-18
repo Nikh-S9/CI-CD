63bb0df087a96c3a89a6afd2a30666eb492febdc
Nikhil S <110578666+Nikh-S9@users.noreply.github.com>
2022-09-19 01:21:49 +0530

    Update ip_app.py

--- ip_app.py
+++ ip_app.py
@@ -1,6 +1,6 @@
-from flask import Flask 
-from flask import jsonify 
-import socket, os 
+from flask import Flask
+from flask import jsonify
+import socket, os
 
 app = Flask(__name__)
 
@@ -13,11 +13,6 @@ def print_the_ip():
 @app.route('/check')
 def print_check():
     return "Yes, application is running - CHECK complete"
-
-@app.route('/health')
-def print_check():
-    resp = jsonify(success=True)
-    return resp
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
